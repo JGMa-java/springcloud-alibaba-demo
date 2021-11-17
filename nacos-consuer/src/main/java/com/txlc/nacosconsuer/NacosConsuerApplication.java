37e30f678aa72950078a17ed4458669e4ee4e0fd
jgma_legion <18638691837@163.com>
2021-11-18 00:00:14 +0800

    nacos消费者

--- nacos-consuer/src/main/java/com/txlc/nacosconsuer/NacosConsuerApplication.java
+++ nacos-consuer/src/main/java/com/txlc/nacosconsuer/NacosConsuerApplication.java
@@ -1,15 +1,17 @@
-package com.txlc.nacosprovider;
+package com.txlc.nacosconsuer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @Author: admin
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class NacosProviderApplication {
-
+public class NacosConsuerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NacosProviderApplication.class, args);
+        SpringApplication.run(NacosConsuerApplication.class, args);
     }
 
 }
---
+++ nacos-provider/src/main/java/com/txlc/nacosprovider/ctrl/ProviderController.java
@@ -0,0 +1,21 @@
+package com.txlc.nacosprovider.ctrl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: admin
+ */
+@RestController
+public class ProviderController {
+    Logger logger= LoggerFactory.getLogger(ProviderController.class);
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam(value = "name",defaultValue = "forezp",required = false)String name){
+
+        return "hi "+name;
+    }
+}
